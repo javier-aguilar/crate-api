261b8c2b4bad9f133ddaf99aff714079b697bbea
Javier Aguilar <jvr.agx@gmail.com>
2020-07-03 11:17:09 -0600

    Update env vars

--- build/config/env.js
+++ build/config/env.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.PORT = exports.NODE_ENV = void 0;
+exports.DATABASE_URL = exports.PORT = exports.NODE_ENV = void 0;
 
 var _dotenv = _interopRequireDefault(require("dotenv"));
 
@@ -17,6 +17,9 @@ _dotenv["default"].config(); // Environment
 var NODE_ENV = process.env.NODE_ENV; // Port
 
 exports.NODE_ENV = NODE_ENV;
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8000; // DB URL
+
 exports.PORT = PORT;
+var DATABASE_URL = process.env.DATABASE_URL;
+exports.DATABASE_URL = DATABASE_URL;
 //# sourceMappingURL=env.js.map
